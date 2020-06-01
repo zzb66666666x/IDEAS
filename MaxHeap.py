b00d48b160c8582433a5fd636b2410708fce6248
zzb66666666x <18767066589@163.com>
2020-06-02 01:34:28 +0800

    updated

--- MaxHeap.py
+++ MaxHeap.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-class MinHeap:
+class MaxHeap:
     # Root: A[1]
     # Node A[n]
     # Node.left A[2n]
@@ -19,35 +19,35 @@ class MinHeap:
         self.heap.append(val)
         self.sift_up(len(self.heap)-1)
 
-    def retrieve_min(self):
+    def retrieve_max(self):
         return self.heap[1]
 
-    def delete_min(self):
+    def delete_max(self):
         if len(self.heap) <= 1:
             return None
-        minimal_val = self.heap[1]
+        maximal_val = self.heap[1]
         self.heap[1] = self.heap[-1]
         self.heap.pop(-1)
         self.sift_down(1)
-        return minimal_val
+        return maximal_val
 
     def sift_down(self, index):
         length = len(self.heap)
         max_index = length - 1
         left = index * 2
         right = index * 2 + 1
-        minimal = index
-        if left <= max_index and self.heap[left] < self.heap[minimal]:
-            minimal = left
-        if right <= max_index and self.heap[right] < self.heap[minimal]:
-            minimal = right
-        if minimal != index:
-            self.heap[minimal], self.heap[index] = self.heap[index], self.heap[minimal]
-            self.sift_down(minimal)
+        maximal = index
+        if left <= max_index and self.heap[left] > self.heap[maximal]:
+            maximal = left
+        if right <= max_index and self.heap[right] > self.heap[maximal]:
+            maximal = right
+        if maximal != index:
+            self.heap[maximal], self.heap[index] = self.heap[index], self.heap[maximal]
+            self.sift_down(maximal)
 
     def sift_up(self, index):
         parent = index // 2
-        if parent >= 1 and self.heap[parent] > self.heap[index]:
+        if parent >= 1 and self.heap[parent] < self.heap[index]:
             self.heap[parent],self.heap[index] = self.heap[index],self.heap[parent]
             self.sift_up(parent)
 
@@ -58,21 +58,21 @@ class MinHeap:
 
 
 if __name__ == "__main__":
-    minheap = MinHeap()
-    minheap.insert(1)
-    minheap.insert(2)
-    minheap.insert(34)
-    minheap.insert(4)
-    minheap.insert(56)
-    minheap.insert(6)
-    minheap.insert(32)
-    print(minheap.heap)
-    while minheap.isEmpty() is False:
-        print(minheap.delete_min())
-    del minheap
+    maxheap = MaxHeap()
+    maxheap.insert(1)
+    maxheap.insert(2)
+    maxheap.insert(34)
+    maxheap.insert(4)
+    maxheap.insert(56)
+    maxheap.insert(6)
+    maxheap.insert(32)
+    print(maxheap.heap)
+    while maxheap.isEmpty() is False:
+        print(maxheap.delete_max())
+    del maxheap
     print("\nanother test!!!\n")
     content = list(range(100))
     np.random.shuffle(content)
-    minheap = MinHeap(content)
-    while minheap.isEmpty() is False:
-        print(minheap.delete_min())
+    maxheap = MaxHeap(content)
+    while maxheap.isEmpty() is False:
+        print(maxheap.delete_max())
